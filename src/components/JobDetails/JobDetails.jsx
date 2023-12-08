a8edf51ab51f52b9d1d22264a01c3fa6b087a2ff
mariyam45 <mariansuchi019915gmail.com>
2023-12-08 22:57:33 +0600

    job details end

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -5,7 +5,7 @@ import "./JobDetails.css";
 
 const JobDetails = () => {
   const { id } = useParams();
-  const filterById = jobData.filter((jd) => jd.id === id);
+  const filterById = jobData.filter((jd) => jd.id == id);
 
   // const setJobDetails = (id) => {
 
@@ -21,7 +21,8 @@ const JobDetails = () => {
     <div>
       <div className="title ">
         <img className="img1" src="../../../assets/images/bg1.png" alt="" />
-        <h2 className=" ">Job details for : {filterById[0]?.job_title}</h2>
+        <h2 className=" ">Job Details </h2>
+
         <img className="img2" src="../../../assets/images/bg2.png" alt="" />
       </div>
 
@@ -30,28 +31,20 @@ const JobDetails = () => {
 
         <div className="details_left w-4/6 mr-4">
           <p>
-            <span>Job Description : </span> A UI/UX (User Interface/User
-            Experience) designer is responsible for designing and creating
-            engaging and effective interfaces for software and web applications.
-            This includes designing the layout, visual design, and interactivity
-            of the user interface.
+            <span>Job Description : </span>
+            {filterById[0]?.job_description}{" "}
           </p>
           <p>
-            <span>Job Responsibility : </span> Collaborating with
-            cross-functional teams: UI/UX designers often work closely with
-            other teams, including product management, engineering, and
-            marketing, to ensure that the user interface is aligned with
-            business and technical requirements. You will need to be able to
-            effectively communicate your design ideas and gather feedback from
-            other team members.
+            <span>Job Responsibility : </span>
+            {filterById[0]?.job_responsibility}
           </p>
           <p>
             <span className="block mb-3 mt-4">Educational Requirements : </span>{" "}
-            Bachelor degree to complete any reputational university.
+            {filterById[0]?.educational_requirements}
           </p>
           <p>
-            <span className="block mb-3 mt-4">Experiences : </span> 2-3 Years in
-            this field.
+            <span className="block mb-3 mt-4">Experiences : </span>
+            {filterById[0]?.experiences}
           </p>
         </div>
         <div className="details_right w-2/6">
@@ -61,15 +54,14 @@ const JobDetails = () => {
             <div className="mb-1">
               <img src="../../../assets/icons/money.png" alt="" />
               <p>
-                <span>Salary : </span> 100K - 150K (Per Month)
+                <span>Salary : </span> {filterById[0]?.salary}
               </p>
             </div>
             <div>
-              <img src="../../../assets/icons/money.png" alt="" />
+              <img src="../../../assets/icons/calendar.png" alt="" />
 
-              <p>
-                <span>Job title : </span>Product Designer
-              </p>
+              <span>Job title : </span>
+              <p>{filterById[0]?.job_title}</p>
             </div>
 
             <p className="titles mt-5">Contact Information</p>
@@ -77,13 +69,14 @@ const JobDetails = () => {
             <div className="mb-1">
               <img src="../../../assets/icons/phone.png" alt="" />
               <p>
-                <span>Phone : </span> 01750-00 00 00
+                <span>Phone : </span>
+                {filterById[0]?.contact_information.phone}
               </p>
             </div>
             <div>
               <img src="../../../assets/icons/email.png" alt="" />
               <p>
-                <span>Email : </span> info@gmail.com
+                <span>Email : </span> {filterById[0]?.contact_information.email}
               </p>
             </div>
             <div className="">
@@ -92,12 +85,12 @@ const JobDetails = () => {
                 alt=""
                 className="me-0"
               />
-              <p>
-                <span>Address :</span> Dhanmondi 32, Sukrabad Dhaka, Bangladesh
-              </p>
+
+              <span>Address :</span>
+              <p className="">{filterById[0]?.contact_information.address}</p>
             </div>
           </div>
-          <button className="btn btn-block">block</button>
+          <button className="btn btn-block">Apply Now</button>
         </div>
       </div>
     </div>
